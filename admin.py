368041856d5f81f019bbbc2f0ad09389ddd3ec81
Deprion <86678150+DEPRESSION2@users.noreply.github.com>
2021-10-27 13:30:46 +0300

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Article, Author, Comment, Like
-
-admin.site.register(Author)
-admin.site.register(Article)
-admin.site.register(Comment)
-admin.site.register(Like)
